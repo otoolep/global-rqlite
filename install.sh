b39a7db7abb75a5672c598a332ffecf252f0e28e
Philip O'Toole <philip.otoole@yahoo.com>
2016-04-09 22:13:43 -0700

    Use release 2.1

--- install.sh
+++ install.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-apt-get update
-apt-get -y install gcc curl make git bison
-bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
-source /root/.gvm/scripts/gvm
-gvm install go1.5 --binary
-gvm use go1.5
 
-mkdir rqlite
-cd rqlite/
-export GOPATH=$PWD
-go get -t github.com/otoolep/rqlite/...
+curl -L  https://github.com/otoolep/rqlite/releases/download/v2.1/rqlited-v2.1-linux-amd64.tar.gz -o rqlited-v2.1-linux-amd64.tar.gz
+tar xvfz rqlited-v2.1-linux-amd64.tar.gz
+cd rqlited-v2.1-linux-amd64
+./rqlited ~/node.1
+
