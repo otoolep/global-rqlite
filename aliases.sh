f7968883f86af06f9254d8553e67bac0a046ffcb
Philip O Toole <philip.otoole@yahoo.com>
2016-05-01 21:08:29 -0700

    Use IP addresses

--- aliases.sh
+++ aliases.sh
@@ -4,7 +4,11 @@ PUBLIC_IP=`curl http://169.254.169.254/latest/meta-data/public-ipv4`
 LOCAL_IP=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
 
 function start_follower {
-    $HOME/rqlited -http -httpadv -raft -raftadv -join $1 $HOME/node
+    if [ -z "$1" ]; then
+        echo "no leader hostname provided"
+        return 1
+    fi
+    $HOME/rqlited -http $LOCAL_IP:4001 -httpadv $PUBLIC_IP:4001 -raft $LOCAL_IP:4002 -raftadv $PUBLIC_IP:4002 -join http://$1:4001 $HOME/node
 }
 
-alias start_leader="$HOME/rqlited -http -httpadv -raft -raftadv  $HOME/node"
+alias start_leader="$HOME/rqlited -http $LOCAL_IP:4001 -httpadv $PUBLIC_IP:4001 -raft $LOCAL_IP:4002 -raftadv $PUBLIC_IP:4002 $HOME/node"
