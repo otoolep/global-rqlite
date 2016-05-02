75762588b249a121d35cf9281d783ed52d252623
Philip O Toole <philip.otoole@yahoo.com>
2016-05-01 21:04:37 -0700

    Access EC2 metadata

--- aliases.sh
+++ aliases.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+PRIVATE_IP=`ifconfig`
+PUBLIC_IP=`curl http://169.254.169.254/latest/meta-data/public-ipv4`
+
 function start_follower {
     $HOME/rqlited -http -httpadv -raft -raftadv -join $1 $HOME/node
 }
