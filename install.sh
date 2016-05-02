d6db6b2e60fa04faae0b90fceb927ca8a8025162
Philip O Toole <philip.otoole@yahoo.com>
2016-05-01 20:58:13 -0700

    Create symlink for binary

--- install.sh
+++ install.sh
@@ -3,6 +3,4 @@
 VERSION=v3.0.1
 curl -L  https://github.com/otoolep/rqlite/releases/download/$VERSION/rqlited-$VERSION-linux-amd64.tar.gz -o rqlited-$VERSION-linux-amd64.tar.gz
 tar xvfz rqlited-$VERSION-linux-amd64.tar.gz
-cd rqlited-$VERSION-linux-amd64
-./rqlited ~/node.1
-
+ln -s rqlited-$VERSION-linux-amd64/rqlited $HOME/rqlited
