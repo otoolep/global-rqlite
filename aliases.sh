cfe8bf9ccb48a68a2dee25c5d77831e77532b910
Philip O Toole <philip.otoole@yahoo.com>
2016-05-01 21:05:28 -0700

    Get local IP

--- aliases.sh
+++ aliases.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PRIVATE_IP=`ifconfig`
 PUBLIC_IP=`curl http://169.254.169.254/latest/meta-data/public-ipv4`
+LOCAL_IP=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
 
 function start_follower {
     $HOME/rqlited -http -httpadv -raft -raftadv -join $1 $HOME/node
