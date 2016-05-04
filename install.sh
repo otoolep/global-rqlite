e655d5d4d2a9005e3999d29c4588ade81a847d28
Philip O'Toole <philip.otoole@yahoo.com>
2016-05-03 21:21:44 -0700

    Upgrade to v3.1.0

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=v3.0.1
+VERSION=v3.1.0
 curl -L -s https://github.com/otoolep/rqlite/releases/download/$VERSION/rqlited-$VERSION-linux-amd64.tar.gz -o rqlited-$VERSION-linux-amd64.tar.gz
 tar xvfz rqlited-$VERSION-linux-amd64.tar.gz
 ln -s rqlited-$VERSION-linux-amd64/rqlited $HOME/rqlited
