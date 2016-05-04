f8f5bfb9746fb66d6558b152d24991e19a5d0d9b
Philip O'Toole <philip.otoole@yahoo.com>
2016-05-03 21:52:39 -0700

    Link to CLI

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ VERSION=v3.1.0
 curl -L -s https://github.com/otoolep/rqlite/releases/download/$VERSION/rqlited-$VERSION-linux-amd64.tar.gz -o rqlited-$VERSION-linux-amd64.tar.gz
 tar xvfz rqlited-$VERSION-linux-amd64.tar.gz
 ln -s rqlited-$VERSION-linux-amd64/rqlited $HOME/rqlited
+ln -s rqlited-$VERSION-linux-amd64/rqlite $HOME/rqlite
